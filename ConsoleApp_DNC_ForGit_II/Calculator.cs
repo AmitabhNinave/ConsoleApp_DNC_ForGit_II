1b98d1f0a78c92a66ea02c7db605c3a4b049aec0
Amitabh Ninave <amitabh.ninave@hotmail.com>
2022-05-10 17:16:07 +0530

    Changes in Calculator.cs file

--- ConsoleApp_DNC_ForGit_II/Calculator.cs
+++ ConsoleApp_DNC_ForGit_II/Calculator.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp_DNC_ForGit_II
 {
-    class Calculator
+    public class Calculator
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DesignationId { get; set; }
+        public int DepartmentId { get; set; }
 
     }
 }
